c0e0fc398f016faf86098293d4fbad955cc7ce73
Brian Tran <briantran3504@gmail.com>
2025-02-22 00:28:29 -0500

    mod6 done

---
+++ mod6-solution/app.js
@@ -0,0 +1,36 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', function ($scope) {
+    $scope.input = "";
+    $scope.message = "";
+    $scope.borderColor = {};
+    $scope.textColor = {};
+
+    $scope.checkIfTooMuch = function () {
+        const items = $scope.input.split(",");
+        var numItems = items.reduce((acc, curr) => {
+            if (curr.trim().length == 0) acc--;
+            return acc;
+        }, items.length);
+
+        if (numItems > 0) {
+            if (numItems > 3) {
+                $scope.message = "Too much!";
+            } else {
+                $scope.message = "Enjoy!";
+            }
+    
+            $scope.borderColor = { 'border' : "1px solid green" };
+            $scope.textColor = { 'color' : "green" };
+        } else {
+            $scope.message = "Please enter data first";
+            $scope.borderColor = { 'border' : "1px solid red" };
+            $scope.textColor = { 'color' : "red" };
+        }
+    };
+});
+
+})();
